ea23b1da0070655193e0462873180a5d736017b0
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-04-15 00:09:50 +0530

    Add get_loaders method to get loaders for Train and Test dataset

---
+++ data_loader.py
@@ -0,0 +1,17 @@
+import torch
+from torchvision import datasets, transforms
+
+def get_loaders(batch_size, num_workers = 0):
+    trans = transforms.Compose([transforms.ToTensor()])
+    trainset = datasets.MNIST('./Data', train=True, transform=trans, download=True)
+    testset = datasets.MNIST('./Data', train=False, transform=trans, download=True)
+
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size, shuffle=True, num_workers=num_workers)
+    testloader = torch.utils.data.DataLoader(testset, batch_size, shuffle=False, num_workers=num_workers)
+
+    return trainloader, testloader
+
+if __name__ == "__main__":
+    trainloader, testloader = get_loaders(batch_size=4)
+
+    print(trainloader.dataset.classes)
