4723bb5dc179e241f60970751fbcdceaaea14a79
vaibhawvipul <vaibhaw.vipul@gmail.com>
2020-04-28 09:41:24 +0530

    training loop for VAE

--- data_loader.py
+++ data_loader.py
@@ -3,8 +3,8 @@ from torchvision import datasets, transforms
 
 def get_loaders(batch_size, num_workers = 0):
     trans = transforms.Compose([transforms.ToTensor()])
-    trainset = datasets.MNIST('./Data', train=True, transform=trans, download=True)
-    testset = datasets.MNIST('./Data', train=False, transform=trans, download=True)
+    trainset = datasets.MNIST('./data', train=True, transform=trans, download=True)
+    testset = datasets.MNIST('./data', train=False, transform=trans, download=True)
 
     trainloader = torch.utils.data.DataLoader(trainset, batch_size, shuffle=True, num_workers=num_workers)
     testloader = torch.utils.data.DataLoader(testset, batch_size, shuffle=False, num_workers=num_workers)
--- draw.py
+++ draw.py
@@ -17,7 +17,7 @@ import torch.optim as optim
 
 import torchvision 
 import torchvision.transforms as transforms
-
+import data_loader
 import os, sys
 
 torch.manual_seed(1) 
@@ -37,10 +37,13 @@ write_glimpse = 5 # table 3
 z_size = 100 # table 3
 num_units_lstm = 256 # table 3
 
+# load data 
+train_loader, test_loader = data_loader.get_loaders(64, 4)
+
 # Model definition
-class LSTMNETWORK(nn.Module):
+class LSTMVAENETWORK(nn.Module):
     def __init__(self, input_size, num_units_lstm):
-        super(LSTMNETWORK, self).__init__()
+        super(LSTMVAENETWORK, self).__init__()
 
         # take the input
         self.encoderrnn = nn.LSTM(input_size, num_units_lstm)
@@ -68,6 +71,48 @@ class LSTMNETWORK(nn.Module):
         x = x.view(-1, img_size)
         mu, logvar = self.encoder(x)
         z = self.reparameterize(mu, logvar)
-        return self.decode(z)
+        return self.decode(z), mu, logvar
 
 
+# Reconstruction + KL divergence losses summed over all elements and batch
+def loss_function(recon_x, x, mu, logvar):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+
+    # see Appendix B from VAE paper:
+    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # https://arxiv.org/abs/1312.6114
+    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+    return BCE + KLD
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = LSTMVAENETWORK(img_size, num_units_lstm).to(device)
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, _ = data
+        
+        inputs.to(device)
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        recon_batch, mu, logvar =  model(inputs)
+        loss = criterion(outputs, inputs)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
